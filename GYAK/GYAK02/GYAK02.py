c5b542811ee4f055426576a03f5198ba0ce96b47
yxtasy <baloghbence123@gmail.com>
2023-03-07 19:00:40 +0100

    c

---
+++ GYAK/GYAK02/GYAK02.py
@@ -0,0 +1,86 @@
+# %%
+import numpy as np
+
+# %%
+#FONTOS!!!
+
+# CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
+
+# %%
+#Készíts egy függvényt ami létre hoz egy nullákkal teli numpy array-t.
+#Paraméterei: mérete (tupel-ként), default mérete pedig legyen egy (2,2)
+#Be: (2,2)
+#Ki: [[0,0],[0,0]]
+#create_array()
+
+# %%
+def create_array(input_tuple =(2,2)):
+    return np.zeros(input_tuple)
+
+
+# %%
+#Készíts egy függvényt ami a paraméterként kapott array-t főátlót feltölti egyesekkel
+#Be: [[1,2],[3,4]]
+#Ki: [[1,2],[3,1]]
+#set_one()
+
+# %%
+def set_one(inputarray):
+    np.fill_diagonal(inputarray, 1)
+    return inputarray
+
+# %%
+# Transzponáld a paraméterül kapott mártix-ot:
+# Be: [[1, 2], [3, 4]]
+# Ki: [[1, 2], [3, 4]]
+# do_transpose()
+
+# %%
+def do_transpose(inputMatrix):
+    np.transpose(inputMatrix)
+    return inputMatrix
+
+# %%
+# Készíts egy olyan függvényt ami az array-ben lévő értékeket N tizenedjegyik kerekíti, alapértelmezetten 
+# Be: [0.1223, 0.1675], n = 2
+# Ki: [0.12, 0.17]
+# round_array()
+
+# %%
+def round_array(input_array,n = 2):
+    np.round(input_array,n,input_array)
+    return input_array
+
+
+# %%
+# Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 0 - False-ra az 1 True-ra cserélni
+# Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
+# Ki: [[ True False False], [ True  True  True], [False False False]]
+# bool_array()
+
+# %%
+def bool_array(input_array):
+    return np.array(input_array, dtype=bool)
+
+
+# %%
+# Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
+# Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
+# Ki: [[ True False False], [ True  True  True], [False False False]]
+# invert_bool_array()
+
+# %%
+def invert_bool_array(input_array):
+    return np.invert(np.array(input_array,dtype=bool))
+
+# %%
+# Készíts egy olyan függvényt ami a paraméterként kapott array-t kilapítja
+# Be: [[1,2], [3,4]]
+# Ki: [1,2,3,4]
+# flatten()
+
+# %%
+def flatten(input_array):
+    return input_array.flatten()
+
+
